c4ce7290e0ade597c5207b150e5296b63d275c08
pepeolo <49265831+pepeolo@users.noreply.github.com>
2022-05-10 14:27:15 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/applogistica/Remitente.java
@@ -0,0 +1,15 @@
+package com.example.applogistica;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Remitente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remitente);
+
+    }
+}
