3bba10a8ecfeb7f353db165759458055a7640b77
pepeolo <49265831+pepeolo@users.noreply.github.com>
2022-05-17 10:20:03 +0200

    Add files via upload
    Se suben resto de archivos java

---
+++ app/src/main/java/com/example/applogistica/Envio.java
@@ -0,0 +1,16 @@
+package com.example.applogistica;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Envio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_envio);
+
+    }
+}
+
